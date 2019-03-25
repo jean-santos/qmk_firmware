d2d3347c198971faa22ede2820064af7c8ee8e68
vagrant <vagrant@vagrant.vm>
2019-03-25 21:15:29 +0000

    Initial commit

---
+++ keyboards/ergodox_ez/keymaps/jyrael/rules.mk
@@ -0,0 +1,8 @@
+STENO_ENABLE = yes
+MOUSEKEY_ENABLE  = no # Mouse keys(+4700)
+SWAP_HANDS_ENABLE= yes # Allow swapping hands of keyboard
+TAP_DANCE_ENABLE = yes
+RGBLIGHT_ENABLE = no
+EXTRAKEY_ENABLE  = no # Audio control and System control(+450)
+CONSOLE_ENABLE   = no # Console for debug(+400)
+COMMAND_ENABLE   = no # Commands for debug and configuration
